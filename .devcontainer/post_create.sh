f663f6f08f38ff59211d9a6be0f8f1717ed45845
shimada <38740976+dev-shimada@users.noreply.github.com>
2026-01-10 19:44:45 +0900

    feature/casks (#4)
    * Refactor .goreleaser.yaml: clean up comments and update cask configuration
    
    * Remove Dockerfile and post_create.sh; update devcontainer.json and .goreleaser.yaml for improved configuration

--- .devcontainer/post_create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# tools
-echo "source /usr/share/bash-completion/completions/git" >> ~/.bashrc
-git config --local core.editor vim
-git config --local pull.rebase false
-
-echo export PATH="$PATH:$(go env GOPATH)/bin" >> ~/.bashrc
